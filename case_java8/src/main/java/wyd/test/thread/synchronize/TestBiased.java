777e6bdc300938ce2768761ad3387e748026776f
王玉东 <1174872613@qq.com>
2023-02-17 11:26:37 +0800

    偏向锁-批量撤销-1

--- case_java8/src/main/java/wyd/test/thread/synchronize/TestBiased.java
+++ case_java8/src/main/java/wyd/test/thread/synchronize/TestBiased.java
@@ -3,10 +3,21 @@ package wyd.test.thread.synchronize;
 import lombok.extern.slf4j.Slf4j;
 import org.openjdk.jol.info.ClassLayout;
 
+import java.util.Vector;
+import java.util.concurrent.locks.LockSupport;
+
 @Slf4j(topic = "c")
 public class TestBiased {
 
+    static Thread t1, t2, t3;
+
     public static void main(String[] args) throws InterruptedException {
+
+    }
+
+    // 测试批量重偏向
+    public static void test1(){
+
         Dog d = new Dog();
 
         new Thread(()->{
@@ -38,6 +49,40 @@ public class TestBiased {
             log.debug(ClassLayout.parseInstance(d).toPrintableSimple());
         },"t2").start();
     }
+
+    // 测试批量撤销
+    public static void test2(){
+        Vector<Dog> list = new Vector<>();
+        int loopNumber = 39;
+
+        t1 = new Thread(()->{
+            for (int i = 0; i < loopNumber; i++) {
+                Dog dog = new Dog();
+                list.add(dog);
+                log.debug(i + "\t" + ClassLayout.parseInstance(dog).toPrintableSimple());
+            }
+            LockSupport.unpark(t2);
+        }, "t1");
+        t1.start();
+
+        t2 = new Thread(()->{
+            LockSupport.park();
+            log.debug("========================>");
+            // 撤销重偏向20次后，批量重偏向t2
+
+            LockSupport.unpark(t3);
+        }, "t2");
+        t2.start();
+
+        t3 = new Thread(()->{
+            LockSupport.park();
+
+
+
+        }, "t3");
+        t3.start();
+    }
+
 }
 
 class Dog {
