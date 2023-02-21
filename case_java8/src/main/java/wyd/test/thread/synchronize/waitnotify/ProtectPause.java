d169b4babcb2ea3649cc363d3cd6174f03fcd012
王玉东 <1174872613@qq.com>
2023-02-21 13:45:32 +0800

    synchronized内容

--- case_java8/src/main/java/wyd/test/thread/synchronize/waitnotify/ProtectPause.java
+++ case_java8/src/main/java/wyd/test/thread/synchronize/waitnotify/ProtectPause.java
@@ -5,12 +5,29 @@ import wyd.test.util.Downloader;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @Slf4j(topic = "c")
 public class ProtectPause {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 3; i++) {
+            new Person().start();
+        }
 
-        GuardedObject guardedObject = new GuardedObject();
+        TimeUnit.SECONDS.sleep(1);
+
+        for (Integer id : MailBoxes.getIds()) {
+            new Postman(id, "内容" + id).start();
+        }
+    }
+
+    public static void test1(){
+        GuardedObject guardedObject = new GuardedObject(null, null);
         new Thread(()->{
             // 消费线程
             try {
@@ -34,13 +51,103 @@ public class ProtectPause {
                 throw new RuntimeException(e);
             }
         }, "t2").start();
+    }
 
+    public static void test2(){
+        GuardedObject guardedObject = new GuardedObject(null, null);
+
+        new Thread(()->{
+            try {
+                log.debug("begin...");
+                Object o = guardedObject.get(2000);
+                log.debug("获取到的结果为：{}", o);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }, "t1").start();
+
+
+        new Thread(()->{
+            log.debug("begin...");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            guardedObject.complete(null);
+        }, "t2").start();
+    }
+
+}
+@Slf4j(topic = "c.Person")
+class Person extends Thread{
+    @Override
+    public void run() {
+        GuardedObject guardedObject = MailBoxes.createGuardedObject();
+        // 开始收信
+        try {
+            log.debug("开始收信...");
+            Object o = guardedObject.get(5000);
+            log.debug("id为: {},收到的内容为: {}", guardedObject.getId(), o);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+@Slf4j(topic = "c.Postman")
+class Postman extends Thread{
+
+    private Integer id;
+    private Object mail;
+
+    public Postman(Integer id, Object mail){
+        this.id = id;
+        this.mail = mail;
+    }
+    @Override
+    public void run() {
+        GuardedObject guardedObject = MailBoxes.getGuardedObject(id);
+        log.debug("送信 id:{}, 内容：{}", id, mail);
+        guardedObject.complete(mail);
+    }
+}
+
+class MailBoxes{
+    private static Map<Integer, GuardedObject> boxes = new ConcurrentHashMap<>();
+    private static AtomicInteger id = new AtomicInteger(0);
+
+    // 取GuardedObject使用
+    public static synchronized GuardedObject createGuardedObject(){
+        GuardedObject guardedObject = new GuardedObject(id.incrementAndGet(), null);
+        boxes.put(guardedObject.getId(), guardedObject);
+        return guardedObject;
+    }
+
+    // 生产GuardedObject使用
+    public static GuardedObject getGuardedObject(Integer id){
+        return boxes.remove(id);
+    }
+
+    public static Set<Integer> getIds(){
+        return boxes.keySet();
     }
 
 }
 
 class GuardedObject{
     private Object response;
+
+    private Integer id;
+
+    public GuardedObject(Integer id, Object response){
+        this.id = id;
+        this.response = response;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
 
     // 获取结果
     public Object get(long timeout) throws InterruptedException {
@@ -57,7 +164,7 @@ class GuardedObject{
                     break;
                 }
                 this.wait(waitTime);
-                passedTime = startTime - System.currentTimeMillis();
+                passedTime = System.currentTimeMillis() - startTime;
             }
         }
         return response;
